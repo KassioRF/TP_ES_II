af792bbb9b25608353f2cc28f0c08f5cf54cd2fc
KassioRF <kassio_rf@hotmail.com>
2022-06-04 16:29:14 -0300

    database imports

--- project/imports.py
+++ project/imports.py
@@ -8,7 +8,7 @@ def imports():
   format = "%Y-%m-%d"
 
 
-  n = -4
+  n = -3
   print(range(n,0))
   date = "2022-06-03"
   dateinit = datetime.strptime(date,format)
@@ -21,22 +21,53 @@ def imports():
   #Ganhos
   
   #salario
-  sal = "Salário"
-  value = 1800
-  dtype = DType.objects.get(dtype=sal)
+  # sal = "Salário"
+  # value = 1800
+  # dtype = DType.objects.get(dtype=sal)
   
-  
-  date = dateend
-  data = Data(mode=profit, value=value, date=date ,dtype=dtype)
-  data.salve()
 
 
 
-  #Spent
-  #Aluguel
+  # Spent
+  # Aluguel
+  # date = "2022-06-07"
+  # dateinit = datetime.strptime(date,format)
+
   # aluguel = "Aluguel"
   # value = 600
-  # dtype = Dtype.objects.get(dtype=aluguel)
+  # dtype = DType.objects.get(dtype=aluguel)
+
+  # date = "2022-05-10"
+  # dateinit = datetime.strptime(date,format)
+
+  # cat = "Internet"
+  # value = 109.90
+  # dtype = DType.objects.get(dtype=cat)
+
+
+  # date = "2022-06-04"
+  # dateinit = datetime.strptime(date,format)
+
+  # cat = "Mercado"
+  # value = 480.20
+  # dtype = DType.objects.get(dtype=cat)
+
+
+
+  # date = "2022-05-15"
+  # dateinit = datetime.strptime(date,format)
+
+  # cat = "Fatura do Cart."
+  # value = 227.64
+  # dtype = DType.objects.get(dtype=cat)
+
+
+  for d in range(n,1):
+    date = dateinit + relativedelta(months=d)
+    data = Data(mode=spent, value=value, date=date ,dtype=dtype)
+    data.save()
+
+
 
   # date = dateend
   # data = Data(mode=spent, value=value, date=date ,dtype=dtype)
